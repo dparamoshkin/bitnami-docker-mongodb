e2f7a7f6c021bebc5a2823bf697480722a2bcec1
Dmitriy Paramoshkin <dmitriy.paramoshkin@gmail.com>
2020-07-27 13:17:53 +0200

    debug

--- 4.2/debian-10/rootfs/opt/bitnami/scripts/mongodb-env.sh
+++ 4.2/debian-10/rootfs/opt/bitnami/scripts/mongodb-env.sh
@@ -54,7 +54,7 @@ done
 unset mongodb_env_vars
 
 # Paths
-export PATH="/usr/bin/mongo:/opt/bitnami/common/bin:$PATH"
+export PATH="/opt/bitnami/mongodb/bin:/opt/bitnami/common/bin:$PATH"
 export MONGODB_VOLUME_DIR="/bitnami"
 export MONGODB_BASE_DIR="/usr/bin/mongo"
 export MONGODB_CONF_DIR="$MONGODB_BASE_DIR/conf"
